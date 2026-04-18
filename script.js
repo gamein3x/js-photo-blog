203269858df408a5fff118cfe27053c8a111caf0
Ivan Caldarella <gamein3x@gmail.com>
2026-04-18 16:48:37 +0200

    Milestone 1 e 2

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+// @ts-check
 `use strict`
+
+const MAIN_API_URL = 'https://lanciweb.github.io/demo/api/pictures/';
+
+/**
+ *  @param {{ id: number, title: string, date: string, url: string}[]} posts
+ */
+
+fetch(MAIN_API_URL)
+    .then(response => {
+        return response.json();
+    }).then(json => {
+        console.log(json);
+    }).catch(error => {
+        console.error(error);
+    }).finally(() => {
+        console.log('Caricato');
+    });
