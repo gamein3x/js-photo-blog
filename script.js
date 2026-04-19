1f0c21a01388eca44f5700a04403a9721028b7f3
Ivan Caldarella <gamein3x@gmail.com>
2026-04-20 00:46:20 +0200

    Milestone 4 - logica aggiornata e adattata in script.js, esercizio finito

--- script.js
+++ script.js
@@ -1,17 +1,46 @@
 // @ts-check
 `use strict`
 
+const placeholder = 'bella';
+
 const MAIN_API_URL = 'https://lanciweb.github.io/demo/api/pictures/';
+
+const cardContainer = document.querySelector('#main-container');
+console.log(cardContainer);
+
 
 /**
  *  @param {{ id: number, title: string, date: string, url: string}[]} posts
  */
+
+function renderCards(posts) {
+    let cardBlockHTML = '';
+
+    for (const post of posts) {
+        cardBlockHTML += `
+<div class="photo-card" id="photo-card${post.id}">
+                <div class="pin-container">
+                    <img src="./assets_day1/img/pin.svg" alt="Pin">
+                </div>
+                <div class="img-container">
+                    <img src="${post.url}" alt="Photo album n.${post.id}: ${post.title}">
+                </div>
+                <p class="photo-description mono">${post.date}</p>
+            </div>
+`;
+    }
+
+    if (cardContainer !== null) {
+        cardContainer.innerHTML = cardBlockHTML;
+    }
+}
 
 fetch(MAIN_API_URL)
     .then(response => {
         return response.json();
     }).then(json => {
         console.log(json);
+        renderCards(json);
     }).catch(error => {
         console.error(error);
     }).finally(() => {
